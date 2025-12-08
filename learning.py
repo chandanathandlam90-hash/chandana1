9197592f9eb4668b9023c0accddec95bcfedd3f0
Sharada1415 <ramyar.alpha@gmail.com>
2025-12-08 11:51:59 +0530

    added for loop range(10, 20), checking even-odd

--- learning.py
+++ learning.py
@@ -1,7 +1,6 @@
-num = int(input("Enter the number : "))
-
-if num%2==0:
-    print(f"{num} is even")
-else:
-    print(f"{num} is odd")
+for num in range(10, 20):
+    if num%2==0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
 
