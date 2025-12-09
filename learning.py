fb9b454eb51af7800a83357944cb3c774a7f2585
Sharada1415 <ramyar.alpha@gmail.com>
2025-12-09 10:19:04 +0530

    created class Numbers

--- learning.py
+++ learning.py
@@ -1,9 +1,13 @@
-def even_odd(start, end):
-    for num in range(start, end):
-        if num%2==0:
-            print(f"{num} is even")
-        else:
-            print(f"{num} is odd")
+class Numbers:
 
-even_odd(100, 200)
+    def even_odd(self, start, end):
+        for num in range(start, end):
+            if num%2==0:
+                print(f"{num} is even")
+            else:
+                print(f"{num} is odd")
+
+
+num = Numbers()
+num.even_odd(100, 200)
 
