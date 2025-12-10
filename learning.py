0660a0ca58b7cbaa2c4d71b6bf2935648e8bd9a4
Sharada1415 <ramyar.alpha@gmail.com>
2025-12-10 10:35:29 +0530

    defined attribute for prime numbers in learning.py

--- learning.py
+++ learning.py
@@ -8,6 +8,43 @@ class Numbers:
                 print(f"{num} is odd")
 
 
+    def prime_numbers(self):
+        for i in range(2, 50):
+            for j in range(2, i):
+                if i%j==0:
+                    break
+            print(i)
+
+
 num = Numbers()
 num.even_odd(100, 200)
+
+#################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
